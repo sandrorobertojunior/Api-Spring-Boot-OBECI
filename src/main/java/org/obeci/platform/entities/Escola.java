acc483cf9090ea76380379f4caf8bd4bc15198b2
sandro <sandrorobertojunior@hotmail.com>
2026-01-09 14:47:53 -0300

    codigo comentado

--- src/main/java/org/obeci/platform/entities/Escola.java
+++ src/main/java/org/obeci/platform/entities/Escola.java
@@ -1,17 +1,8 @@
 package org.obeci.platform.entities;
-
 import jakarta.persistence.*;
 import lombok.Data;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.Type;
-import org.hibernate.type.SqlTypes;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @Entity
